5ac462c0510bec66bd207d83e5fc05af344462ad
gyxing <172176234@qq.com>
2018-10-13 17:02:09 +0800

    update

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
 #!/usr/bin/env node
+const fs = require('fs');
 const program = require('commander');
 const download = require('download-git-repo');
+const handlebars = require('handlebars');
+const inquirer = require('inquirer');
+const ora = require('ora');
+const chalk = require('chalk');
+const symbols = require('log-symbols');
 
-program.version('1.0.0', '-v, --version')
+const _VERSION_ = '0.0.1';  // 版本号
+
+program.version(_VERSION_, '-v, --version')
     .command('init <name>')
     .action((name) => {
-        console.log(name)
-    });
+        if(!fs.existsSync(name)){
+            inquirer.prompt([
+                {
+                    name: 'name',
+                    message: 'Project name',
+                    default: name
+                }, {
+                    name: 'description',
+                    message: 'Project description',
+                    default: 'A Node.js Simple Project'
+                }, {
+                    name: 'author',
+                    message: 'Author',
+                    default: 'anonymity'
+                }
+            ]).then((answers) => {
+                const spinner = ora('downloading template...');
+                spinner.start();
+                download('direct:https://github.com/gyxing/nodejs-simple.git#master', name, {clone: true}, (err) => {
+                    if(err){
+                        spinner.fail();
+                        console.log(symbols.error, chalk.red(err));
+                    }else{
+                        spinner.succeed();
+                        const fileName = `${name}/package.json`;
+                        const meta = {
+                            name: answers.name,
+                            description: answers.description,
+                            author: answers.author
+                        }
+                        if(fs.existsSync(fileName)){
+                            const content = fs.readFileSync(fileName).toString();
+                            const result = handlebars.compile(content)(meta);
+                            fs.writeFileSync(fileName, result);
+                        }
+                        console.log(symbols.success, chalk.green('Project initialization finished!'));
+                    }
+                })
+            })
+        }else{
+            // 错误提示项目已存在，避免覆盖原有项目
+            console.log(symbols.error, chalk.red('Project already exists!'));
+        }
+    })
 program.parse(process.argv);
